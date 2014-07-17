72904d17b8bdd596857a91a8bd1415668c97548c
enmanuel.cueva <enmanuel.cueva@kurtgeiger.com>
2014-07-17 10:56:51 +0100

    structure set up ready as kata start point

---
+++ src/Sorter.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace BubbleSort;
+
+class Sorter {
+
+    /**
+     * @param array $input
+     *
+     * @return array
+     */
+    public function run( array $input )
+    {
+        return $input;
+    }
+
+} 
--- tests/BubbleSortTest.php
+++ tests/BubbleSortTest.php
@@ -1,6 +1,56 @@
 <?php
 
+use \BubbleSort\Sorter as BubbleSort;
 
-class BubbleSortTest extends PHPUnit_Framework_TestCase {
+class BubbleSortTest extends PHPUnit_Framework_TestCase
+{
+
+    /** @var  BubbleSort */
+    protected $bubbleSort;
+
+    public function setUp()
+    {
+        $this->bubbleSort = new BubbleSort();
+    }
+
+    /**
+     * @param array $input
+     * @param array $expected
+     *
+     * @internal     param array $collection
+     *
+     * @dataProvider collectionsProvider
+     */
+    public function testBubbleSortAlgorithmResults(array $input, array $expected)
+    {
+        $result = $this->bubbleSort->run($input);
+        $this->assertEquals($expected, $result);
+    }
+
+
+    /*****************************************************************************************************************
+     *                                                    PROVIDERS
+     *****************************************************************************************************************/
+
+    /**
+     * @return array
+     */
+    public function collectionsProvider()
+    {
+        return [
+            [
+                [ 2, 3, 1 ], [ 1, 2, 3 ]
+            ],
+            [
+                [ 8, 7, 6, 2, 1 ], [ 1, 2, 6, 7, 8 ]
+            ],
+            [
+                [ 1, 2, 3, 4 ], [ 1, 2, 3, 4 ]
+            ],
+            [
+                [ 2, 6, 3, 4, 5, 1 ], [ 1, 2, 3, 4, 5, 6 ]
+            ]
+        ];
+    }
 
 } 
