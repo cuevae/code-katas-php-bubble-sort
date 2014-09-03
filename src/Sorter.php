af375c607983ecd0e0fe02c7942c5556800998f0
Leanne Hill <Leanne.Hill@kurtgeiger.com>
2014-09-03 12:14:14 +0100

    adding bubble sort example

--- src/Sorter.php
+++ src/Sorter.php
@@ -11,6 +11,36 @@ class Sorter {
      */
     public function run( array $input )
     {
+    	$changed = true;
+    	
+    	$k = count($input);
+    	
+        for($i = 0; $i < $k; $i++)
+        {
+        	if($changed)
+        	{
+        		$changed = false;
+        		
+	        	for($j = 0; $j < $k-1; $j++)
+	        	{
+	        		if($input[$j] > $input[$j+1])
+	        		{
+	        			$temp = $input[$j];
+	        			
+	        			$input[$j] = $input[$j+1];
+	        			
+	        			$input[$j+1] = $temp;
+	        			
+	        			$changed = true;
+	        		}
+	        	}  	
+	        }
+	        else 
+	        {
+	        	break;
+	        }
+        }
+        
         return $input;
     }
 
